18c33da1d2a30faaed68b4de6ad27d35ca9d1cba
Matthijs Kooijman <matthijs@stdin.nl>
2018-06-19 12:41:34 +0200

    Expand calibration calculation

--- controller-sketch/controller-sketch.ino
+++ controller-sketch/controller-sketch.ino
@@ -215,16 +215,17 @@ void queueUplink() {
 void setup() {
     battery[0] = new Battery();
     battery[0]->attachFlowController(0, 8, 23);
-    battery[0]->attachLevelController(0, A0, 25, 0.28, -43);
-    battery[0]->attachLevelController(1, A1, 27, 0.30, -59);
-    battery[0]->attachLevelController(2, A2, 29, 0.32, -46);
+    // 5000mV, 1023 steps, 100Ohm, 0.154mA/cm. 4mA/0.154 - 20cm
+    battery[0]->attachLevelController(0, A0, 25, 5000.0/1023/100/0.176, -4/0.176 - 20);
+    battery[0]->attachLevelController(1, A1, 27, 5000.0/1023/100/0.165, -4/0.165 - 35);
+    battery[0]->attachLevelController(2, A2, 29, 5000.0/1023/100/0.154, -4/0.154 - 20);
     battery[0]->attachFlowController(1, 5);
 
     battery[1] = new Battery();
     battery[1]->attachFlowController(0, 4, 31);
-    battery[1]->attachLevelController(0, A3, 33, 0.28, -39);
-    battery[1]->attachLevelController(1, A4, 35, 0.31, -49);
-    battery[1]->attachLevelController(2, A5, 37, 0.32, -39);
+    battery[1]->attachLevelController(0, A3, 33, 5000.0/1023/100/0.176, -4/0.176 - 16);
+    battery[1]->attachLevelController(1, A4, 35, 5000.0/1023/100/0.159, -4/0.159 - 24);
+    battery[1]->attachLevelController(2, A5, 37, 5000.0/1023/100/0.154, -4/0.154 - 13);
     battery[1]->attachFlowController(1, 3);
 
     for (size_t b=0; b<2; b++) {
